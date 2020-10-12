1b6a618ef6dd0b624ca769b242f9ee5ebe0b29b8
Milena Monova <milena@fresh-range.com>
2020-10-12 08:10:20 +0300

    Added listing gigs

--- pages/gigs.js
+++ pages/gigs.js
@@ -5,7 +5,6 @@ import {
 } from "../components/Store";
 
 import { useContext, useState, useEffect } from "react";
-import { BigNumber, ethers } from "ethers";
 
 import { router, useRouter } from "next/router";
 
@@ -21,72 +20,78 @@ function Gigs() {
   const router = useRouter();
 
   useEffect(() => {
-    setOpenGigs([
-      {
-        _id: '01emd4t27a7emmz1e8twr8dr33', 
-        title: "gig1",
-        offeredDitos: 24,
-        description: "lorem ipsum blabla",
-        neededSkills: ["a", "b", "c"],
-      },
-    ]);
+    async function effect() {
+      let resp = await fetch(
+        `http://3.250.29.111:3005/api/gig?isOpen=false`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: 'Bearer ' + 'WyIweGY3MTMxOGQ5MDQ4YmIwZDA3YTRlNWIxOWM3ZWNjZjBkYzcyZjA4MGE5NTdhNGZhNmQxZTAwNmFkNTNmYWViYTc2Njg4NzI2OWI0MTg5NGE0YmE3OGNmOWNkYjk0NWIxYmFmYTI0ZTY0NmY1NjNmZjAxNWQ0OTEwNjhkODEyMWQwMWMiLCJ7XCJpYXRcIjoxNjAyNDc4NTc0LFwiZXh0XCI6MTYwMjQ3OTQ3NCxcImlzc1wiOlwiZGlkOmV0aHI6MHgzZjEzQUEzNzhFYzA3ZjVENDMyZGVmMjg5YWZlQTU1ZERiYUJFM0Q0XCIsXCJzdWJcIjpcIjFNaDdPM2NmRXU0bGN6am9lYkw3YWNxVVZiU2NRNXRkSU0wV1pWa3B6Mk09XCIsXCJhdWRcIjpcImRpZDptYWdpYzowZTAwMjI0OS0wYmY0LTQ2ODMtOWFlNi0zZDUxMjhhYzQyNTZcIixcIm5iZlwiOjE2MDI0Nzg1NzQsXCJ0aWRcIjpcImM2MzUwZTljLTBiYTItNDczZS1hYTIwLWZmYmQzMjczM2ZlYVwiLFwiYWRkXCI6XCIweGUwODY3YWUwNDBjNzI0ZDVlYjU4NGM2MDQ4MjU0NDMxZjBmODFlYmNjZGFhNTlhYWQwNmFkMDhkNzg1YjgyM2Q3YTlmZjdhOGM0OWJkZmI3NjU4ZjM1Y2Q4NGRhYmFhMjI5MTBiODBlOGYwYTFjMjViYjVhZDAxMDdkODRjYzQwMWJcIn0iXQ==',
+            'Content-Type': 'application/json'
+          },
+        });
+      const openGigsResp = await resp.json();
+      return openGigsResp;
+    }
+    effect().then(openGigsResp => setOpenGigs(openGigsResp));
   }, []);
 
-  const takeGig = async () => {
-    let result = await fetch(
-      `http://3.250.29.111:3005/api/gig/${openGigs[0]._id}/accept`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: token,
-          'Content-Type': 'application/json'
-        },
-      }
-    );
-  }
+const takeGig = (gigID) => {
+  let result = fetch(
+    `http://3.250.29.111:3005/api/gig/${gigID}/accept`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: 'Bearer ' + 'WyIweGY3MTMxOGQ5MDQ4YmIwZDA3YTRlNWIxOWM3ZWNjZjBkYzcyZjA4MGE5NTdhNGZhNmQxZTAwNmFkNTNmYWViYTc2Njg4NzI2OWI0MTg5NGE0YmE3OGNmOWNkYjk0NWIxYmFmYTI0ZTY0NmY1NjNmZjAxNWQ0OTEwNjhkODEyMWQwMWMiLCJ7XCJpYXRcIjoxNjAyNDc4NTc0LFwiZXh0XCI6MTYwMjQ3OTQ3NCxcImlzc1wiOlwiZGlkOmV0aHI6MHgzZjEzQUEzNzhFYzA3ZjVENDMyZGVmMjg5YWZlQTU1ZERiYUJFM0Q0XCIsXCJzdWJcIjpcIjFNaDdPM2NmRXU0bGN6am9lYkw3YWNxVVZiU2NRNXRkSU0wV1pWa3B6Mk09XCIsXCJhdWRcIjpcImRpZDptYWdpYzowZTAwMjI0OS0wYmY0LTQ2ODMtOWFlNi0zZDUxMjhhYzQyNTZcIixcIm5iZlwiOjE2MDI0Nzg1NzQsXCJ0aWRcIjpcImM2MzUwZTljLTBiYTItNDczZS1hYTIwLWZmYmQzMjczM2ZlYVwiLFwiYWRkXCI6XCIweGUwODY3YWUwNDBjNzI0ZDVlYjU4NGM2MDQ4MjU0NDMxZjBmODFlYmNjZGFhNTlhYWQwNmFkMDhkNzg1YjgyM2Q3YTlmZjdhOGM0OWJkZmI3NjU4ZjM1Y2Q4NGRhYmFhMjI5MTBiODBlOGYwYTFjMjViYjVhZDAxMDdkODRjYzQwMWJcIn0iXQ==',
+        'Content-Type': 'application/json'
+      },
+    }
+  );
+}
 
-  return (
-    <div className="w-full h-screen p-8">
-      <h1>Open Gigs</h1>
-      <div className="grid grid-cols-3 gap-8">
-        {openGigs.map((gig, i) => {
-          return (
-            <div
-              key={i}
-              className="border-2 border-blue-600 flex flex-col shadow"
-            >
-              <div className="flex justify-between border-b-2 border-gray-400 p-2">
-                <p>{gig.title}</p>
-                <p>DiTo offered: {gig.offeredDitos}</p>
-              </div>
-              <p className="p-2">{gig.description}</p>
-              <div className="flex border-t-2 border-gray-400 p-2">
-                <p>Skills needed: </p>
-                {gig.neededSkills.map((skill, j) => (
-                  <span>{`#${skill}`}</span>
-                ))}
-              </div>
-              <button type="button" className="bg-red-600 text-white w-full" onClick={takeGig}>
-                Take this gig!
-              </button>
+return (
+  <div className="w-full h-screen p-8">
+    <h1>Open Gigs</h1>
+    <div className="grid grid-cols-3 gap-8">
+      {openGigs.map((gig, i) => {
+        return (
+          <div
+            key={i}
+            className="border-2 border-blue-600 flex flex-col shadow"
+          >
+            <div className="flex justify-between border-b-2 border-gray-400 p-2">
+              <p>{gig.title}</p>
+              <p>DiTo offered: {gig.creditsOffered}</p>
             </div>
-          );
-        })}
-      </div>
-      <div className="flex justify-between mt-8">
-        <button
-          type="button"
-          className="p-4 border-2 border-blue-600"
-          onClick={() => router.push("/createGig")}
-        >
-          Create new gig
+            <p className="p-2">{gig.description}</p>
+            <div className="flex border-t-2 border-gray-400 p-2">
+              <p>Skills needed: </p>
+              {gig.skills.map((skill, j) => (
+                <span>{`#${skill}`}</span>
+              ))}
+            </div>
+                
+            <button type="button" className="bg-red-600 text-white w-full" onClick={() => takeGig(gig._id)}>
+              Take this gig!
+              </button>
+          </div>
+        );
+      })}
+    </div>
+    <div className="flex justify-between mt-8">
+      <button
+        type="button"
+        className="p-4 border-2 border-blue-600"
+        onClick={() => router.push("/createGig")}
+      >
+        Create new gig
         </button>
-        <div className="flex flex-col p-4 border-2 border-blue-600">
-          <h2>Your skills</h2>
-        </div>
+      <div className="flex flex-col p-4 border-2 border-blue-600">
+        <h2>Your skills</h2>
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default Gigs;
