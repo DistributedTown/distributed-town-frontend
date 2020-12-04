00bc04854781af338f0c29c1a22b21ac3d6a91ec
Lorenzo Bersano <lbersano@themis.it>
2020-12-04 15:15:36 +0100

    Fixed provider url

--- pages/api/getFunded.js
+++ pages/api/getFunded.js
@@ -3,8 +3,10 @@ import { ethers } from "ethers";
 export default async function handler(req, res) {
   try {
     //TODO move in env
-    const INFURA_PROVIDER_URL =
-      "https://ropsten.infura.io/v3/6a96a40694e24de3b6313b71ca71786b";
+    const PROVIDER_URL =
+      process.env.BLOCKCHAIN === "ETH"
+        ? "https://ropsten.infura.io/v3/6a96a40694e24de3b6313b71ca71786b"
+        : `https://rpc-mainnet.maticvigil.com/v1/${process.env.MATICVIGIL_PROJECT_ID}`;
     const WALLET_MNEMONIC =
       "welcome parade tired spin security depend spy move three clerk loyal lunar";
 
@@ -15,14 +17,14 @@ export default async function handler(req, res) {
 
     let tx = {
       to: publicAddress,
-      value: ethers.utils.parseEther("0.1")
+      value: ethers.utils.parseEther("0.1"),
     };
 
     // Signing a transaction
     await walletMnemonic.signTransaction(tx);
     // The connect method returns a new instance of the
     // Wallet connected to a provider
-    const provider = new ethers.providers.JsonRpcProvider(INFURA_PROVIDER_URL);
+    const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL);
     let wallet = walletMnemonic.connect(provider);
 
     // Sending ether
