d0d298adc701df611305e58190191c19551438ea
Dimitar Dulev <dulev.dimitar@gmail.com>
2020-12-16 18:25:18 +0200

    QR display real community name

--- pages/skillwallet/qr.js
+++ pages/skillwallet/qr.js
@@ -2,23 +2,29 @@ import { QRCode } from 'react-qrcode-logo';
 import { useGetUserInfo } from '../../hooks/useGetUserInfo';
 import { useGetDitoBalance } from '../../hooks/useGetDitoBalance';
 import Card from '../../components/Card';
+import { useGetCommunity } from '../../hooks/useGetCommunity';
 
 function QR() {
   const { data: userInfo } = useGetUserInfo();
   const { data: ditoBalance } = useGetDitoBalance();
+  const { data: community } = useGetCommunity();
 
   return (
     <div className="flex w-full h-screen">
       <div className="flex flex-col px-40 py-64 justify-center items-center bg-denim w-1/2">
         <div className="bg-ripe-lemon w-56 h-56 p-4 flex justify-center items-center rounded-xl">
-          <QRCode
-            value={userInfo._id}
-            logoImage="/isologo.svg"
-            logoWidth={60}
-            logoHeight={60}
-            bgColor="transparent"
-            size={180}
-          />
+          {userInfo ? (
+            <QRCode
+              value={userInfo ? userInfo._id : '...'}
+              logoImage="/isologo.svg"
+              logoWidth={60}
+              logoHeight={60}
+              bgColor="transparent"
+              size={180}
+            />
+          ) : (
+            'Loading QR code.'
+          )}
         </div>
         <Card className="bg-white p-2 mt-10">
           Scan Wallet’s QR-Code to verify new member.
@@ -28,13 +34,13 @@ function QR() {
         <Card className="flex flex-col justify-center items-center">
           <h3 className="text-2xl">Accepting Community</h3>
           <Card filled className="my-4">
-            <p>DITO #23</p>
+            <p>{community ? community.communityInfo.name : '...'}</p>
           </Card>
           <h3 className="text-2xl mb-4">Accepting Member</h3>
           <div className="bg-black p-4 flex justify-around items-center rounded-xl w-full">
             <p className="text-white flex flex-col">
               <span className="text-4xl">👨</span>
-              <span>{userInfo._id}</span>
+              <span>{userInfo ? userInfo._id : '...'}</span>
             </p>
             <div className="flex flex-col w-1/2 p-3 items-center justify-start text-white">
               <span>DiTo</span>
