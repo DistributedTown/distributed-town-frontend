74ec959d2fd2c9c22028226e8bb711bb491d2db2
mchrupcala <michael.chrupcala@gmail.com>
2021-05-21 10:09:38 -0400

    First run of changes to choose community in Join flow

--- components/CommunityCard.js
+++ components/CommunityCard.js
@@ -9,17 +9,28 @@ function CommunityCard({ onSelectCommunity, selected, community }) {
       className="flex flex-col space-y-4 cursor-pointer"
       onClick={onSelectCommunity}
     >
-      <Card outlined className="grid items-center grid-cols-2 gap-4">
+      {/* <Card outlined className="grid items-center grid-cols-2 gap-4">
         <h2 className="font-bold">{name}</h2>
         <Button filled disabled={selected || members === 24}>
           {selected ? 'Selected' : 'Select'}
         </Button>
-      </Card>
-      <div className="grid grid-cols-4">
-        <p>Members:</p>
-        <p>{members}</p>
-        <p>Scarcity score:</p>
-        <p>{scarcityScore}</p>
+      </Card> */}
+      <div className="flex">
+        <div classsName="w-1/2 bg-gray-500">
+          The Dark DiTo
+        </div>
+        <div className="w-1/2 bg-gray-50">
+          Accepting new diTizens?
+        </div>
+      </div>
+
+      <div 
+      // className="grid grid-cols-4"
+      >
+        The greatest Description you could think of. A story about passion, cooperation, 
+        conflict & chaos - unveiling word by word, a deeper truth about humankind. The
+        greatest DDescription you could think  of. A story about passion, cooperation, 
+        conflict & chaos - unveiling word by word.
       </div>
     </Card>
   );
