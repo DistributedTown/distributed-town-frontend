5586fc274051aa6f1191b1e41733b9f5fc702962
mchrupcala <michael.chrupcala@gmail.com>
2021-05-22 00:06:04 -0400

    Community card and choose community styling updates

--- components/CommunityCard.js
+++ components/CommunityCard.js
@@ -1,25 +1,28 @@
-import Button from './Button';
 import Card from './Card';
 
-function CommunityCard({ onSelectCommunity, selected, community }) {
-  const { name, members, scarcityScore } = community;
+function CommunityCard({ onSelectCommunity}) {
 
   return (
     <Card
-      className="flex flex-col space-y-4 cursor-pointer"
+      className="flex flex-col space-y-4 cursor-pointer border-2 rounded-3xl border-denim mb-4"
       onClick={onSelectCommunity}
     >
-      <Card outlined className="grid items-center grid-cols-2 gap-4">
-        <h2 className="font-bold">{name}</h2>
-        <Button filled disabled={selected || members === 24}>
-          {selected ? 'Selected' : 'Select'}
-        </Button>
-      </Card>
-      <div className="grid grid-cols-4">
-        <p>Members:</p>
-        <p>{members}</p>
-        <p>Scarcity score:</p>
-        <p>{scarcityScore}</p>
+      <div className="flex">
+        <div className="w-1/2 bg-white h-16 border-2 rounded-xl border-denim flex justify-evenly items-center">
+          <img alt="Dito Black Logo"></img>
+          <h3 className="underline bold text-xl">The Dark DiTo</h3>
+        </div>
+        <div className="w-1/2 bg-gray-50">
+          Accepting new diTizens?
+        </div>
+      </div>
+
+      <div 
+      >
+        The greatest Description you could think of. A story about passion, cooperation, 
+        conflict & chaos - unveiling word by word, a deeper truth about humankind. The
+        greatest DDescription you could think  of. A story about passion, cooperation, 
+        conflict & chaos - unveiling word by word.
       </div>
     </Card>
   );
